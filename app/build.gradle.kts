a9a12dba202d5bed17d6388171a718ebd577b645
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-05-20 22:17:16 -0500

    Activitie uno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -35,7 +37,6 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
-        compose = true
         viewBinding = true
     }
 }
--- app/src/main/java/com/arias/brandon/laboratoriocalificado02/Ejercicio01Activity.kt
+++ app/src/main/java/com/arias/brandon/laboratoriocalificado02/Ejercicio01Activity.kt
@@ -1,20 +1,30 @@
 package com.arias.brandon.laboratoriocalificado02
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.arias.brandon.laboratoriocalificado02.databinding.ActivityEjercicio01Binding
 
 class Ejercicio01Activity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEjercicio01Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_ejercicio01)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityEjercicio01Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Ocultar inicialmente el View verde (opcional)
+        binding.cuadroVerde.visibility = View.GONE
+
+        // Mostrar el cuadro verde
+        binding.botonMostrar.setOnClickListener {
+            binding.cuadroVerde.visibility = View.VISIBLE
+        }
+
+        // Ocultar el cuadro verde
+        binding.botonOcultar.setOnClickListener {
+            binding.cuadroVerde.visibility = View.GONE
         }
     }
 }
